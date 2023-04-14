21e6481c6c647ef7e93be6f6b3d42efd90e72c80
Ricard Magnet <ricardmagnet@gmail.com>
2023-04-14 13:48:29 +0200

    idioma

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/test',[TestController::class ,'index']);
